dd4aac16b97305050f3ade31dc635357b352f4b6
Your Name <you@example.com>
2020-01-14 17:33:51 -0800

    further study

--- markov.py
+++ markov.py
@@ -1,5 +1,6 @@
 """Generate Markov text from text files."""
 
+import sys
 from random import choice
 from collections import defaultdict
 
@@ -14,11 +15,11 @@ def open_and_read_file(file_path):
     # your code goes here
     with open(file_path, 'r') as f: # 'r' = read-only
         text = f.read().replace("\n", " ") # read the open file then replace the new-line breaks with whitespaces. 
-     
+
     return text
 
 
-def make_chains(text_string):
+def make_chains(text_string, n=2):
     """Take input text as string; return dictionary of Markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -49,18 +50,23 @@ def make_chains(text_string):
     # your code goes here
     words = text_string.split() # split by whitespace 
 
-    for w1, w2, w3 in zip(words[:-2], words[1:-1], words[2:]):
-        bi_gram = (w1, w2)
-        chains[bi_gram].append(w3) # if the bigram doesn't exit in the keys, append w3
+    # for w1, w2, w3 in zip(words[:-2], words[1:-1], words[2:]):
+    #     bi_gram = (w1, w2)
+    #     chains[bi_gram].append(w3) # if the bigram doesn't exit in the keys, append w3
         # if bi_gram in chains:
         #   chains[bi_gram].append(w3)
         # else:
         #   chains[bi_gram] = [w3]
 
+    for i in range(len(words)-n):
+        n_gram = tuple(words[i:(i+n)])
+        value_for_n_gram = words[i+n]
+        chains[n_gram].append(value_for_n_gram)
+
     return chains
 
 
-def make_text(chains):
+def make_text(chains, n=2):
     """Return text from chains."""
 
     words = []
@@ -71,22 +77,24 @@ def make_text(chains):
             start_bigram = choice(list(chains.keys()))
             words.extend(list(start_bigram))
         else:
-            possible_next_words = chains[(words[-2], words[-1])]
+            possible_next_words = chains[tuple(words[-n:])]
             if not possible_next_words:
                 return " ".join(words)
             next_word = choice(possible_next_words)
             words.append(next_word)
 
 
-input_path = "green-eggs.txt"
+#input_path = "green-eggs.txt"
+
+n = 6
 
 # Open the file and turn it into one long string
-input_text = open_and_read_file(input_path)
+input_text = open_and_read_file(sys.argv[1])
 
 # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains(input_text, n=n)
 
 # Produce random text
-random_text = make_text(chains)
+random_text = make_text(chains, n=n)
 
 print(random_text)
